47709b7ee42036396a7ff1d0fa863d9061b3639a
Maciek Zych aka pgn666 <maciek.zych@gmail.com>
2021-11-14 16:32:04 +0100

    use redis INCR command to incerment key values for pairs and triplets

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const { debugMode } = require('redis');
+
 console.log("Hello world");
+
+helpers = require('./lib/helpers');
+
+helpers.die(helpers, 'helpers');
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 var fs = require('fs'),
     sys = require('util'),
-    redis = require('redis'),
     process = require('process'),
-    client =  redis.createClient();
+    client = require('./lib/redisClient'),
+    helpers = require('./lib/helpers');
 
-redis.debug_mode = true;
-
-var __die__ = (a,b) => {
-    console.log(b);
-    console.dir(a);
-    process.exit();
+/**
+ * Flush redis database
+ */
+ var flushDB = (mode) => {
+    helpers.debug('Flushing DB');
+    client.flushdb((err, success) => {
+        success ? helpers.debug(success, 'success') : helpers.debug(error, 'error');
+        if ('exit' === mode){
+            console.log('process.exit');
+            process.exit();
+        }
+    });
 }
 
+/**
+ * Reads /texts dir and sets valiues for pairs fpund
+ * 
+ */
 var init = function() {
     var texts = fs.readdirSync(__dirname + '/texts');
     var s, l = texts.length;
@@ -19,46 +29,60 @@ var init = function() {
         var filename = __dirname + '/texts/' + texts[i];
         if( filename.indexOf( '.txt' ) > -1) {
             fs.readFile(filename, 'utf-8', function(err, data) {
+                helpers.debug(data, 'file data');
                 var words = data.split(/\s+/);
                 for(var j = 0; j < words.length - 1; j++) {
-                    console.log(words[j], words[j+1]);
-                    client.set(words[j], words[j+1], redis.print);
+                    var key = words[j] + ":" + words[j+1];
+                    console.log('key', key)
+                    client.INCR(key);
                 }
             });
         }
     }
 }
 
-var init_3 = function(){
+/**
+ * reads '/tests/lab' folder and creats data for paris, whre first item a a pair is atullay two words
+ */
+var init_triplets = function(flush) {
     let texts = fs.readdirSync(__dirname + '/texts/lab');
     let s, w, l = texts.length;
 
-    client.flushdb( function (err, succeeded) {
-        console.log(succeeded); // will be true if successfull
-    });
-
     for(let i = 0; i < l; i++) {
         let filename = __dirname + '/texts/lab/' + texts[i];
-        if(filename.indexOf('.txt')>-1){
-            console.log('filename: ' + filename);
+        if(filename.indexOf('.txt') > -1) {
+            helpers.debug('filename: ' + filename);
             fs.readFile(filename, 'utf-8', function(err, data) {
-                console.log(data, 'data');
+                helpers.debug(data, 'data');
                 let words = data.split(/\s+/);
                 for(var j = 0; j < words.length - 2; j++) {
-                    s = words[j] + ' ' + words[j+1];
-                    w = words[j+2];
-                    console.log(s + '- ' + w);
-                    client.set(s, w, redis.print);
+                    var key = words[j] + ' ' + words[j+1] +  ":" + words[j+2];
+                    console.log('key', key)
+                    client.INCR(key);
                 }
             });
         }
     }
 }
 
-console.dir(process.argv[2], "Mode: ");
+const mode = process.argv[2];
+const withFlush = !!process.argv[3]
 
-if (process.argv[2] == 'init') {
-    init();
-} else {
-    init_3();
+helpers.debug("Mode: ", mode);
+helpers.debug("With additional Flush: ", withFlush);
+
+withFlush ? flushDB() : helpers.debug('no additinal flush DB run');
+
+switch(mode) {
+    case '2':
+        init();
+        break;
+    case '3':
+        init_triplets();
+        break;
+    case 'flushDB':
+        flushDB('exit');
+        break;
+    default:
+        helpers.debug('available modes "2" - init pairs, "3" - init triplets and  "flushDB"')
 }
